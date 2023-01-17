46e4a6f77437b5b6924e9c1d5289f160aaeeb429
leandro <fahurleandro@gmail.com>
2023-01-16 19:59:37 -0800

    its never to late to turn back or restart again

--- widgets/src/components/Navbar/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import "./index.css";
-
-const Navbar = () => {
-  return <div></div>;
-};
-
-export default Navbar;
