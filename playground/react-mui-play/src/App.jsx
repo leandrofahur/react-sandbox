644cc80cf0f5a7cdaabd900a873251f45d743831
leandro <fahurleandro@gmail.com>
2023-04-20 14:03:57 -0700

    Outer Stepper with business logic in App component

--- playground/react-mui-play/src/App.jsx
+++ playground/react-mui-play/src/App.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import OuterStepper from "./components/OuterStepper/OuterStepper";
 import "./App.scss";
 
 const steps = [
-  "Select campaign settings",
-  "Create an ad group",
-  "Create an ad",
+  {
+    label: "First item",
+    innerSteps: [
+      {
+        label: "Subitem 1",
+      },
+    ],
+  },
+  {
+    label: "Second item",
+    innerSteps: [
+      {
+        label: "Subitem 1",
+      },
+      {
+        label: "Subitem 2",
+      },
+      {
+        label: "Subitem 3",
+      },
+    ],
+  },
+  {
+    label: "Third item",
+    innerSteps: [],
+  },
 ];
-
 function App() {
-  return <OuterStepper steps={steps} />;
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNextOuterStep = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handlePreviousOuterStep = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  return (
+    <OuterStepper
+      outerSteps={steps}
+      activeStep={activeStep}
+      handleNextOuterStep={handleNextOuterStep}
+      handlePreviousOuterStep={handlePreviousOuterStep}
+    />
+  );
 }
 
 export default App;
--- playground/react-mui-play/src/components/OuterStepper/OuterStepper.jsx
+++ playground/react-mui-play/src/components/OuterStepper/OuterStepper.jsx
@@ -6,44 +6,41 @@ import { Button, Box, Stepper, Step, StepLabel } from "@mui/material";
 import styles from "./OuterStepper.module.scss";
 
 function OuterStepper(props) {
-  const { steps } = props;
-  const [activeStep, setActiveStep] = useState(0);
+  const {
+    outerSteps,
+    activeStep,
+    handleNextOuterStep,
+    handlePreviousOuterStep,
+  } = props;
 
-  const isLastStep = activeStep === steps.length - 1;
+  const isLastStep = activeStep === outerSteps.length - 1;
   const isFirstStep = activeStep === 0;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handlePrevious = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
 
   return (
     <Box className={styles.boxStepStyling}>
       <Stepper activeStep={activeStep} orientation="vertical">
-        {steps.map((label, index) => {
+        {outerSteps.map((outerStep, index) => {
           const stepProps = {};
           const labelProps = {};
+
+          console.log(outerStep);
 
           // console.log({ stepProps, labelProps });
 
           return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
+            <Step key={outerStep.label} {...stepProps}>
+              <StepLabel {...labelProps}>{outerStep.label}</StepLabel>
             </Step>
           );
         })}
       </Stepper>
       <Box className={styles.boxCtaStyling}>
-        <Button disabled={isFirstStep} onClick={handlePrevious}>
+        <Button disabled={isFirstStep} onClick={handlePreviousOuterStep}>
           Previous
         </Button>
         <Button
           variant="contained"
-          onClick={handleNext}
-          sx={{ ml: 1 }}
+          onClick={handleNextOuterStep}
           disabled={isLastStep}
         >
           Next
@@ -54,7 +51,9 @@ function OuterStepper(props) {
 }
 
 OuterStepper.propTypes = {
-  steps: PropTypes.array.isRequired,
+  outerSteps: PropTypes.array.isRequired,
+  handleNextOuterStep: PropTypes.func.isRequired,
+  handlePreviousOuterStep: PropTypes.func.isRequired,
 };
 
 export default OuterStepper;
